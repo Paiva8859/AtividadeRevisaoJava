3f58bc6a76c62460aaccba72224672d3fe675373
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-03-20 14:22:57 -0300

    20/03/23

--- AtividadeRevisao/AtividadeRevisao.java
+++ AtividadeRevisao/AtividadeRevisao.java
@@ -1,9 +1,19 @@
 package AtividadeRevisaoJava.AtividadeRevisao;
 
+import java.util.Scanner;
+
 /**
  * AtividadeRevisao
  */
 public class AtividadeRevisao {
 
+    Scanner sc = new Scanner(System.in);
+
+
+    public void exemplo1(){
     
+
+
+    }
+
 }
