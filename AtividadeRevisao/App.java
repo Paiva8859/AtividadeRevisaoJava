9f4aec535b20cf5f2df0869e6f120a61d601121e
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-03-20 15:11:41 -0300

    20/03/23

--- AtividadeRevisao/App.java
+++ AtividadeRevisao/App.java
@@ -1,7 +1,28 @@
 package AtividadeRevisaoJava.AtividadeRevisao;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+
+        int exe = 1;
+
+        while (exe > 0) {
+
+        System.out.println("Qual objeto deseja executar? Para cancelar digite 0");
+        exe = sc.nextInt();
+
+        switch (exe){ 
+            case 1:
+                AtividadeRevisao obj1 = new AtividadeRevisao();
+                obj1.exemplo1();
+            break;
+        
+        }
+
+        }
+        sc.close();
     }
 }
--- AtividadeRevisao/AtividadeRevisao.java
+++ AtividadeRevisao/AtividadeRevisao.java
@@ -11,8 +11,44 @@ public class AtividadeRevisao {
 
 
     public void exemplo1(){
-    
+        System.out.println("Informe a nota 1 do aluno");
+        double nota1 = sc.nextDouble();
+        System.out.println("Informe a nota 2 do aluno");
+        double nota2 = sc.nextDouble();
+        double media = (nota1 + nota2)/2;
+        if (media>= 50) {
+            System.out.println( "A média do aluno é: " + media + ". O aluno está aprovado por media");
+        } else {
+            System.out.println( "A média do aluno é: " + media + ". O aluno está reprovado por media");
+        }
+        
+        System.out.println("Informe o total de aulas dadas");
+        double aulas = sc.nextDouble();
 
+        System.out.println("Informe o número de faltas do aluno");
+        double faltas = sc.nextDouble();
+
+        double x = (100*faltas)/aulas; //porcentagem de frequencia
+
+        System.out.println( "O aluno compareceu em "+ (100 - x) + "% das aulas" );
+
+        if (100-x >=75) {
+            System.out.println("O aluno está aprovado por frequência");
+        } else {
+        System.out.println("O aluno está reprovado por frequência");           
+        }
+
+        if (media >=50 && (100 - x) >= 75) {
+            System.out.println("O aluno está aprovado no curso!");
+        }
+        else if (media < 50 && (100 - x) >= 75) {
+            System.out.println("O aluno não possui media alta o suficiente para ser aprovado, mas pode realizar a recuperaçao");
+        } 
+        else {
+            System.out.println("O aluno está reprovado no curso!");
+        }
+
+        System.out.println("================================================");
 
     }
 
